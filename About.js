c4598ea83561f04ebc4d17ff6a19e5b6ab6f04a2
Samyuktha-krishna <88240500+Samyuktha-krishna@users.noreply.github.com>
2021-11-02 21:03:29 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function About() {
+  return (
+    <div>
+      <h1>About page</h1>
+      <p>This is a website where you can access any app directly with one touch.<br />
+               This allows you to save time by providing all options at one place. We use Javascript , css to build this.<br />
+               Happy learning!  </p>
+    </div>
+  );
+}
+
+export default About;
---
+++ Shop.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+
+function Shop() {
+       
+  return (
+    <div>
+            <h1>Shop page</h1>  
+            <h2>Groceries</h2>
+            <h2>Food</h2>
+            <h2>Accesories</h2>
+            <h2>Home appliances</h2>  
+    </div>
+  );
+}
+
+export default Shop;
